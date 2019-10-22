1fbef08604dad631565bd08dea3472afe79b7ca5
Sosuno <ewa.jasinska.02@gmail.com>
2019-10-22 15:18:01 +0200

    added header auth

--- src/main/java/com/iqlearning/rest/APIController.java
+++ src/main/java/com/iqlearning/rest/APIController.java
@@ -8,6 +8,7 @@ import com.iqlearning.rest.resource.LoginForm;
 import com.iqlearning.rest.resource.RegisterForm;
 import com.iqlearning.rest.resource.Token;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,8 @@ public class APIController {
 
 
     @PostMapping("/user/token")
-    public ResponseEntity<String> login(@RequestBody LoginForm loginForm) {
+    public ResponseEntity<?> login(@RequestBody LoginForm loginForm) {
+
         acc = new AccountManagement(userService,sessionService);
         LoggedUser user = acc.login(loginForm.getUsername(), loginForm.getPassword());
         if(user == null) {
@@ -37,26 +39,43 @@ public class APIController {
             return new ResponseEntity<>("Bad password", HttpStatus.FORBIDDEN);
         } else if(user.getId() == -2) {
             return new ResponseEntity<>("Account suspended", HttpStatus.FORBIDDEN);
-        } else return new ResponseEntity<>(user.getSessionID(), HttpStatus.OK);
+        } else return auth(user.getSessionID());
     }
 
     @PostMapping("/user/register")
-    public ResponseEntity<String> registerUser(@RequestBody RegisterForm registerForm) {
+    public ResponseEntity<?> registerUser(@RequestBody RegisterForm registerForm) {
         acc = new AccountManagement(userService,sessionService);
         LoggedUser user = acc.register(registerForm.getUsername(),registerForm.getPassword(),registerForm.getEmail(),registerForm.getName(),registerForm.getSurname());
         if(user.getId() == -1) {
             return new ResponseEntity<>( "Username taken", HttpStatus.NOT_ACCEPTABLE);
         } else if(user.getId() == -2) {
             return new ResponseEntity<>( "Email taken", HttpStatus.NOT_ACCEPTABLE);
-        } else return new ResponseEntity<>(user.getSessionID(), HttpStatus.OK);
+        } else return auth(user.getSessionID());
+
     }
 
     @PostMapping("/user/refresh")
-    public ResponseEntity<LoggedUser> getUserByToken(@RequestBody Token token) {
+    public ResponseEntity<?> getUserByToken(@RequestBody Token token) {
         acc = new AccountManagement(userService,sessionService);
         LoggedUser loggedUser = acc.loginWithSession(token.getToken());
         if(loggedUser.getId() != -1) {
-            return new ResponseEntity<>(loggedUser, HttpStatus.OK);
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.set("Authorization",
+                    loggedUser.getSessionID());
+            return ResponseEntity.ok()
+                    .headers(responseHeaders)
+                    .body(loggedUser);
         } else return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<?> auth(String session) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Authorization",
+                session);
+        Token token = new Token();
+        token.setToken(session);
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body(token);
     }
 }
