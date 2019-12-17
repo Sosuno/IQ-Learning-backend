3906f1e539fdba4bef8da509d45187e2287d3b07
Detk Detkowski <detek@wp.pl>
2019-12-17 18:51:52 +0000

    Resolve "Endpointy"

---
+++ src/main/java/com/iqlearning/context/utils/FullTest.java
@@ -0,0 +1,90 @@
+package com.iqlearning.context.utils;
+
+import com.iqlearning.database.entities.Test;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class FullTest {
+
+    private Long id;
+    private Long owner;
+    private Long subject;
+    private List<FilledQuestion> questions;
+    private boolean shareable;
+    private Timestamp created;
+    private Timestamp lastEdited;
+    private int downloads;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Long owner) {
+        this.owner = owner;
+    }
+
+    public Long getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Long subject) {
+        this.subject = subject;
+    }
+
+    public List<FilledQuestion> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<FilledQuestion> questions) {
+        this.questions = questions;
+    }
+
+    public boolean isShareable() {
+        return shareable;
+    }
+
+    public void setShareable(boolean shareable) {
+        this.shareable = shareable;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getLastEdited() {
+        return lastEdited;
+    }
+
+    public void setLastEdited(Timestamp lastEdited) {
+        this.lastEdited = lastEdited;
+    }
+
+    public int getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(int downloads) {
+        this.downloads = downloads;
+    }
+
+    public FullTest(Test t, List<FilledQuestion> questionList) {
+        this.id = t.getId();
+        this.owner = t.getOwner();
+        this.subject = t.getSubject();
+        this.questions = questionList;
+        this.shareable = t.isShareable();
+        this.created = t.getCreated();
+        this.lastEdited = t.getLastEdited();
+        this.downloads = t.getDownloads();
+    }
+}
