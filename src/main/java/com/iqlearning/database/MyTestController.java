10d11e2f68e9b9e6eb5b253fb80e343a12d2d682
Sosuno <ewa.jasinska.02@gmail.com>
2020-01-12 20:10:18 +0100

    deleted useless lines and changed a way delete works on questions

--- src/main/java/com/iqlearning/database/MyTestController.java
+++ src/main/java/com/iqlearning/database/MyTestController.java
@@ -295,6 +295,4 @@ public Comment addComment() {
 
         return list;
     }
-
-
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,6 +28,7 @@ INSERT INTO questions(owner, subject,question,choice_test,shareable,created,last
   (6,2,'Which one is a prime number?', TRUE,true,now(),now()),
   (6,2,'Mark had 32 euros, Amy had 88 zlotys, Megan had 13 pounds and Sam had 55 dollars. If Mark, Amy and Megan were to buy dollars with their money and give them to Sam, he would have 184 dollars. 1 euro costs 1,1 dollars and one zloty costs 0,3 dollars. Calculate how many pounds you can buy with one dollar.', FALSE,true,now(),now()),
   (6,2,'Ann had 14 apples, John had 27 oranges, Susan had 13 pears and Gary had X grapes. Combined price of all their fruits was 139$. If Ann were to sell 3 apples and Susan were to buy 2 pears, the combined price would not change. If Gary ate 3 grapes and John bought 7 more oranges, the price would rise by 7$. Calculate the price of each of the fruits and the number of grapes.', FALSE,true,now(),now());
+
 INSERT INTO answers(question_id,answer,correct,created,last_edited) VALUES
   (3,'3',FALSE,now(),now()),
   (3,'10',FALSE,now(),now()),
@@ -60,8 +61,6 @@ INSERT INTO answers(question_id,answer,correct,created,last_edited) VALUES
   (9,'15',FALSE,now(),now()),
   (9,'23',TRUE,now(),now()),
   (9,'27',FALSE,now(),now());
-
-<<<<<<< src/main/resources/data.sql
 
 INSERT INTO article_tags(tag) VALUES
   ('Educationl'),
